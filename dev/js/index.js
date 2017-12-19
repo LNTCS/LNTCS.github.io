33da983654062a647031b1042acec9aa8523e5b8
LNTCS <lcs7958@gmail.com>
2017-12-19 17:41:35 +0900

    복사 부분 수정중

--- dev/js/index.js
+++ dev/js/index.js
@@ -244,14 +244,13 @@ $(document).ready(function () {
 	/**
 	 * Copy and copyright
 	 */
-// 	function setClipboardData(str) {
-// 		$('.post-content').on('copy', function (e) {
-// 			var data = window.clipboardData;
-// // || e.originalEvent.clipboardData;
-// 			data.setData('text/plain', str);
-// 			e.preventDefault();
-// 		});
-// 	}
+	function setClipboardData(str) {
+		$('.post-content').on('copy', function (e) {
+			var data = window.clipboardData || e.originalEvent.clipboardData;
+			data.setData('text/plain', str);
+			e.preventDefault();
+		});
+	}
 	$('.post-content').on('mouseup', function (e) {
 		var txt = window.getSelection();
 		if (txt.toString().length >= 30) {
